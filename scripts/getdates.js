b7845de37da1798e2f528d92086c6ddb96143d3e
dariusscotland <dscotland@byupathway.edu>
2025-11-08 18:52:05 -0400

    Home Page assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// JavaScript statement to dynamically output the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = currentYear;
+
+// JavaScript statement to dynamically output the document's last modified date
+document.getElementById("lastModified").innerHTML = document.lastModified;
